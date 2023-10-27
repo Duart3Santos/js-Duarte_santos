b2a0444f31921bd49c6607dd166d1f1004e8c1b0
Gabriel Duarte dos Santos <148454088+Duart3Santos@users.noreply.github.com>
2023-10-27 15:13:25 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
 function setup() {
-    createCanvas(600, 600);
-    background("white");
+  createCanvas(600, 600);
+  background("black");
+}
+
+function draw() {
+  stroke("purple");
+  fill("white");
+
+  // console.log(mouseIsPressed)
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
   }
-  
-  function draw() {
-    stroke("black");
-    fill("blue");
-  
-    // console.log(mouseIsPressed)
-  
-    if (mouseIsPressed) {
-      rect(mouseX, mouseY, 20, 35);
-    }
-  }
-   
+}
+ 
